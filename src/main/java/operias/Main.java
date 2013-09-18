6143a8cf29eb7d56a0b65cee562d07c29250b71e
soosterwaal <sebas@MacBook-Air-van-Sebastiaan.local>
2013-09-18 10:50:57 +0200

    Inital project

---
+++ src/main/java/operias/Main.java
@@ -0,0 +1,17 @@
+package operias;
+
+/**
+ * Main class of operias
+ * @author sebas
+ *
+ */
+public class Main {
+
+	/**
+	 * Start up the operias tool
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
