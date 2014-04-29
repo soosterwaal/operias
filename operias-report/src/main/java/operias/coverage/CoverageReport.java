cc62e43e58fecd986b1de8c6e8b153b1dececd94
soosterwaal <sebas@MacBook-Air-van-Sebastiaan.local>
2014-04-29 11:32:25 +0200

    Initial set up for multie module

--- operias-report/src/main/java/operias/coverage/CoverageReport.java
+++ operias-report/src/main/java/operias/coverage/CoverageReport.java
@@ -3,7 +3,6 @@ package operias.coverage;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
