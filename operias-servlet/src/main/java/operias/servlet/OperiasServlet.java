ef3cd458a48bad1f4e64f859972a11a4ab44925c
soosterwaal <sebas@MacBook-Air-van-Sebastiaan.local>
2014-05-01 10:54:48 +0200

    Added parts to construct the git message and some tests

--- operias-servlet/src/main/java/operias/servlet/OperiasServlet.java
+++ operias-servlet/src/main/java/operias/servlet/OperiasServlet.java
@@ -8,27 +8,35 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import operias.test.general.NoExitSecurityManager;
+
 import org.apache.commons.io.IOUtils;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class OperiasServlet extends AbstractHandler {
 
+	static String username, password;
 	/**
 	 * Run the servlet
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		int serverPort = 8080;
-		if (args.length > 0) {
-			serverPort = Integer.parseInt(args[0]);
+		if (args.length > 2) {
+			serverPort = Integer.parseInt(args[2]);
 		}
 		
+		username = args[0];
+		password = args[1];
+		
+
+        System.setSecurityManager(new NoExitSecurityManager());
+        
 		Server server = new Server(serverPort);
 	    server.setHandler(new OperiasServlet());
 	    
@@ -55,8 +63,12 @@ public class OperiasServlet extends AbstractHandler {
 		JsonParser parser = new JsonParser();
 		
 		// Get the object containig all information
-		JsonObject object = parser.parse(inputData).getAsJsonObject();
-	
+		final JsonObject object = parser.parse(inputData).getAsJsonObject();
+		
+		Thread operiasThread = new Thread("Operias") { public void run() { new Operias(object); }};
+		operiasThread.start();
+		
+		
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
